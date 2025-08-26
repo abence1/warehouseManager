73c34658bfe87cdbd18d5e89dad64de94746fb26
abence1 <acsbence25@gmail.com>
2025-08-26 23:40:07 +0200

    CMake setup

---
+++ build/CMakeFiles/WarehouseMananger.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WarehouseMananger.dir/src/main.cpp.o"
+  "CMakeFiles/WarehouseMananger.dir/src/main.cpp.o.d"
+  "WarehouseMananger"
+  "WarehouseMananger.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WarehouseMananger.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
